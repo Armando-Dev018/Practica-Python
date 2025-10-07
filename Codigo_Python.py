7ed33c55657726dfa2e2c344652d56f10e2533eb
castan.reyes.jorge@gmail.com <castan.reyes.jorge@gmail.com>
2025-10-06 18:50:13 -0600

    first commit

---
+++ Codigo_Python.py
@@ -0,0 +1,11 @@
+# Pedir el primer número
+num1 = float(input("Ingresa el primer número: "))
+
+# Pedir el segundo número
+num2 = float(input("Ingresa el segundo número: "))
+
+# Calcular la suma
+suma = num1 + num2
+
+# Mostrar el resultado
+print(f"La suma de {num1} y {num2} es {suma}")
