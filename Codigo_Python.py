e4c3742bd1b3ab1c9e71c7e5d37e0cf2323e4f2d
castan.reyes.jorge@gmail.com <castan.reyes.jorge@gmail.com>
2025-10-06 18:56:48 -0600

    modificacion

--- Codigo_Python.py
+++ Codigo_Python.py
@@ -1,11 +1,2 @@
-# Pedir el primer número
-num1 = float(input("Ingresa el primer número: "))
-
-# Pedir el segundo número
-num2 = float(input("Ingresa el segundo número: "))
-
-# Calcular la suma
-suma = num1 + num2
-
-# Mostrar el resultado
-print(f"La suma de {num1} y {num2} es {suma}")
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}!")
